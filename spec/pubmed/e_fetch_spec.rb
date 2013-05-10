7214d8c7148a4e9d2f0357ed2fe13e16076c02e3
Scott Pullen <s.pullen05@gmail.com>
2013-05-10 13:14:00 -0400

    adding test for EFetch.build_fetch_uri

--- spec/pubmed/e_fetch_spec.rb
+++ spec/pubmed/e_fetch_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe Pubmed::EFetch do
-  
+  before(:each) do
+    @response_xml = Nokogiri::XML(File.open(File.expand_path('spec/fixtures/efetch_results.xml')))
+    @expected_result = @response_xml.xpath('.//PubmedArticle').map { |element| Pubmed::Article.new(element) }
+    Pubmed::EFetch.stub(:get_response).and_return(@response_xml)
+  end
+
+  describe '.build_fetch_uri' do
+    it 'should build the fetch uri for the paramters' do
+      ids = ['23650637', '23645694']
+      expected = URI.parse(Pubmed::EFETCH_URI)
+      expected.query = URI.encode_www_form({:db => 'pubmed', :retmode => 'xml', :id => ids.join(',')})
+      Pubmed::EFetch.build_fetch_uri(ids, {}).should == expected
+    end
+  end
 end
